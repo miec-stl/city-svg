1dc9f060fc5911ca809ab5f10ed0777f9717154e
Mitch Eagles <mitch.eagles.stl@gmail.com>
2019-11-05 07:31:23 -0600

    Add popups to wards for data

--- app/src/components/CitySVG.jsx
+++ app/src/components/CitySVG.jsx
@@ -5,6 +5,37 @@ import 'leaflet/dist/leaflet.css'
 const StlLatitude = 38.627003;
 const StlLongitude = -90.199402
 
+const TurnoutData = {
+    Ward1:21.3,
+    Ward2:24.4,
+    Ward3:20.5,
+    Ward4:20.7,
+    Ward5:19.4,
+    Ward6:26.6,
+    Ward7:25.1,
+    Ward8:31.8,
+    Ward9:22.7,
+    Ward10:24,
+    Ward11:21.4,
+    Ward12:29.5,
+    Ward13:23.8,
+    Ward14:22.9,
+    Ward15:30.9,
+    Ward16:33.0,
+    Ward17:21.3,
+    Ward18:21.1,
+    Ward19:19.7,
+    Ward20:19.8,
+    Ward21:23.4,
+    Ward22:19.4,
+    Ward23:29.0,
+    Ward24:26.4,
+    Ward25:17.7,
+    Ward26:25,
+    Ward27:20.4,
+    Ward28:29.4
+}
+
 class CitySVG extends Component {
     constructor(props) {
         super(props);        
@@ -20,20 +51,31 @@ class CitySVG extends Component {
     ReadGeoJsonFile(GeoJsonData) {
         L.geoJSON(GeoJsonData, {
             style: (feature) => {
-                const Ward = feature.properties.WARD10;
+                const WardNum = feature.properties.WARD10;
 
                 // TODO: Find better way to randomize ward coloring
                 // Also, this is where we'll do styling for turnout proportion etc.
-                const Color_R = (Ward % 3 * 60);
-                const Color_B = (Ward % 4 * 40);
-                const Color_G = (Ward % 5 * 20);
+                const Color_R = (WardNum % 3 * 60);
+                const Color_B = (WardNum % 4 * 40);
+                const Color_G = (WardNum % 7 * 20);
                 const RBGString = `rgb(${Color_R},${Color_G},${Color_B})`;
 
-                return {weight:1, color:RBGString};
+                return {
+                    color:RBGString,
+                    weight:1, 
+                    stroke: true
+                };
             },
 
             onEachFeature: (feature, layer) => {
-                // TODO: Could add data pop-ups to each feature
+                const WardNum = feature.properties.WARD10;
+                const PrecinctNum = feature.properties.PREC10;
+                const ThisWardTurnout = TurnoutData["Ward"+WardNum];
+
+                let WardInfo = "<div style='font-weight:bold;'>Ward "+WardNum+" Precinct "+PrecinctNum+"</div>"
+                let TurnoutLine = "<p>Turnout in Ward "+WardNum+" was "+ThisWardTurnout+"%</p>";
+                let PopupText = WardInfo.concat(TurnoutLine);
+                layer.bindPopup(PopupText);
             }
         }).addTo(this.state.Map);
     }
